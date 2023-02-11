5ccfe3739d02f6bc0a39a8e2d85e3edbfab82d00
arunsachan <66840949+arunsachan@users.noreply.github.com>
2023-02-11 18:17:22 +0530

    'done'

--- script.js
+++ script.js
@@ -8,20 +8,46 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+ arr.map((item)=>{
+    if(item.marks>50){
+      console.log(item);
+    }
+ })
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((item)=>{
+    if(item.marks>50){
+      console.log(item);
+    }
+  })
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newStud={id:4,name:"susan",age:"20",marks:45};
+  arr.push(newStud);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+    arr.forEach((item)=>{
+    if(item.marks>50){
+      console.log(item);
+    }
+  })
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let newArr = [
+    { id: 1, name: "Amar", age: "28", marks: 40 },
+    { id: 2, name: "Akbar", age: "30", marks: 45 },
+    { id: 3, name: "Anthony", age: "29", marks: 55 },
+  ]; 
+
+  let childArr=arr.concat(newArr);
+  console.log(childArr);
 }
